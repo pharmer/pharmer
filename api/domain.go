390b71152cdcdc7416ab45da4e23c3efd36da974
tamal <tamal@appscode.com>
2017-08-15 00:12:21 -0700

    added-all

--- api/domain.go
+++ api/domain.go
@@ -5,6 +5,10 @@ type DomainManager interface {
 	Domain(cluster string) string
 	ExternalDomain(cluster string) string
 	InternalDomain(cluster string) string
+	WebhookAuthenticationURL(cluster string) string
+	WebhookAuthorizationURL(cluster string) string
+	PublicAPIHttpEndpoint(cluster string) string
+	CompassIPs() []string
 }
 
 type NullDomainManager struct {
@@ -12,6 +16,10 @@ type NullDomainManager struct {
 
 var _ DomainManager = &NullDomainManager{}
 
-func (NullDomainManager) Domain(cluster string) string         { return "" }
-func (NullDomainManager) ExternalDomain(cluster string) string { return "" }
-func (NullDomainManager) InternalDomain(cluster string) string { return "" }
+func (NullDomainManager) Domain(cluster string) string                   { return "" }
+func (NullDomainManager) ExternalDomain(cluster string) string           { return "" }
+func (NullDomainManager) InternalDomain(cluster string) string           { return "" }
+func (NullDomainManager) WebhookAuthenticationURL(cluster string) string { return "" }
+func (NullDomainManager) WebhookAuthorizationURL(cluster string) string  { return "" }
+func (NullDomainManager) PublicAPIHttpEndpoint(cluster string) string    { return "" }
+func (NullDomainManager) CompassIPs() []string                           { return []string{} }
