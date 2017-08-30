82b441d49f4488b91a4a85bfb379b8c0ad7e2e13
Tamal Saha <tamal@appscode.com>
2017-08-30 05:53:55 -0700

    Delete unused cloud connector methods. (#84)

--- cloud/providers/digitalocean/cloud.go
+++ cloud/providers/digitalocean/cloud.go
@@ -8,13 +8,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/appscode/go/crypto/rand"
 	"github.com/appscode/go/errors"
 	"github.com/appscode/pharmer/api"
 	"github.com/appscode/pharmer/cloud"
 	"github.com/appscode/pharmer/credential"
 	"github.com/digitalocean/godo"
 	"golang.org/x/oauth2"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 const containerOsImage = "appscode-containeros"
@@ -61,175 +61,19 @@ func (conn *cloudConnector) IsUnauthorized() (bool, string) {
 	return false, ""
 }
 
-func (conn *cloudConnector) getInstanceImage() (string, error) {
-	imgPage := 0
-	const imgPageSize = 20
-	for {
-		imgs, _, err := conn.client.Images.ListUser(gtx.TODO(), &godo.ListOptions{
-			Page:    imgPage,
-			PerPage: imgPageSize,
-		})
-		if err != nil {
-			return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-		cloud.Logger(conn.ctx).Debugln("List user images")
-		for _, img := range imgs {
-			cloud.Logger(conn.ctx).Debugln(img.ID, "__", img.Name, "---", img.Distribution, "---", img.Type)
-			if img.Name == containerOsImage && img.Distribution == "Debian" {
-				found := false
-				for _, region := range img.Regions {
-					if region == conn.cluster.Spec.Region {
-						found = true
-						cloud.Logger(conn.ctx).Debugf("Image already exists in region %v.", conn.cluster.Spec.Region)
-						return strconv.Itoa(img.ID), nil
-					}
-				}
-				if !found {
-					_, _, err := conn.client.ImageActions.Transfer(gtx.TODO(), img.ID, &godo.ActionRequest{
-						"type":   "transfer",
-						"region": conn.cluster.Spec.Region,
-					})
-					if err != nil {
-						return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-					}
-
-					cloud.Logger(conn.ctx).Infof("Started image transfer to region %v.", conn.cluster.Spec.Region)
-					// wait for the transfer to complete
-					conn.waitForTransfer(img.ID)
-					return strconv.Itoa(img.ID), nil
-				}
-			}
-		}
-		imgPage++
-		if len(imgs) < imgPageSize {
-			break
-		}
-	}
-
-	cloud.Logger(conn.ctx).Info("Creating droplet to build custom image")
-	droplet, _, err := conn.client.Droplets.Create(gtx.TODO(), &godo.DropletCreateRequest{
-		Name:              rand.WithUniqSuffix("kubernetes"),
-		Region:            conn.cluster.Spec.Region,
-		Size:              "512mb",
-		Image:             godo.DropletCreateImage{Slug: "debian-8-x64"},
-		PrivateNetworking: false,
-		IPv6:              false,
-		// http://serverfault.com/a/783795/167143
-		UserData: `#!/bin/bash
-sed -i 's/^GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1 /' /etc/default/grub
-sed -i 's/^GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1 /' /etc/default/grub.d/50-cloudimg-settings.cfg
-update-grub`,
-	})
-	if err != nil {
-		return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-	cloud.Logger(conn.ctx).Info("Wait for custom image instance to become active")
-	conn.waitForInstance(droplet.ID, "active")
-	time.Sleep(30 * time.Second)
-
-	cloud.Logger(conn.ctx).Info("Power off custom image instance")
-	_, _, err = conn.client.DropletActions.PowerOff(gtx.TODO(), droplet.ID)
-	if err != nil {
-		return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-	conn.waitForInstance(droplet.ID, "off")
-
-	cloud.Logger(conn.ctx).Info("Start taking custom image snapshot")
-	_, _, err = conn.client.DropletActions.Snapshot(gtx.TODO(), droplet.ID, containerOsImage)
-	if err != nil {
-		return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-
-	cloud.Logger(conn.ctx).Info("Wait for custom image snapshot to be completed")
-	for {
-		action, err := conn.findSnapshotAction(droplet.ID)
-		if err != nil {
-			return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-		if action.Status == "completed" {
-			break
-		}
-		cloud.Logger(conn.ctx).Debugln(".")
-		time.Sleep(5 * time.Second)
-	}
-
-	var k8sImage *godo.Image
-	snaps, _, err := conn.client.Droplets.Snapshots(gtx.TODO(), droplet.ID, &godo.ListOptions{
-		Page:    0,
-		PerPage: 1, // there can be only one snapshot for the new custom image droplet
-	})
-	if err != nil {
-		return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-	if len(snaps) == 1 {
-		if snaps[0].Name == containerOsImage {
-			k8sImage = &snaps[0]
-		}
-	}
-	cloud.Logger(conn.ctx).Debugln("New K8s base image id", k8sImage.ID, ", name: ", k8sImage.Name)
-
-	_, err = conn.client.Droplets.Delete(gtx.TODO(), droplet.ID)
-	if err != nil {
-		return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-	cloud.Logger(conn.ctx).Info("Delete custom image instance")
-	return strconv.Itoa(k8sImage.ID), nil
-}
-
 func (conn *cloudConnector) waitForInstance(id int, status string) error {
 	attempt := 0
-	for true {
-		cloud.Logger(conn.ctx).Infof("Checking status of instance %v", id)
+	return wait.PollImmediate(cloud.RetryInterval, cloud.RetryTimeout, func() (bool, error) {
+		attempt++
+
 		droplet, _, err := conn.client.Droplets.Get(gtx.TODO(), id)
 		if err != nil {
-			return errors.FromErr(err).WithContext(conn.ctx).Err()
+			return false, nil
 		}
-		cloud.Logger(conn.ctx).Debugf("Instance status %v, %v", droplet, err)
+		cloud.Logger(conn.ctx).Infof("Attempt %v: Instance %v is in status", attempt, id, droplet.Status)
 		if strings.ToLower(droplet.Status) == status {
-			break
+			return true, nil
 		}
-		cloud.Logger(conn.ctx).Infof("Instance %v (%v) is %v, waiting...", droplet.Name, droplet.ID, droplet.Status)
-		attempt += 1
-		time.Sleep(30 * time.Second)
-	}
-	return nil
-}
-
-func (conn *cloudConnector) findSnapshotAction(id int) (*godo.Action, error) {
-	actions, _, err := conn.client.Droplets.Actions(gtx.TODO(), id, &godo.ListOptions{
-		Page:    0,
-		PerPage: 100,
+		return false, nil
 	})
-	if err != nil {
-		return nil, err
-	}
-	for _, a := range actions {
-		if a.Type == "snapshot" {
-			return &a, nil
-		}
-	}
-	return nil, fmt.Errorf("snapshot action not found for droplet %v", id)
-}
-
-func (conn *cloudConnector) waitForTransfer(id int) error {
-	cloud.Logger(conn.ctx).Infof("Wait for the transfer to complete")
-	attempt := 0
-	for {
-		img, _, err := conn.client.Images.GetByID(gtx.TODO(), id)
-		if err != nil {
-			return err
-		}
-		for _, r := range img.Regions {
-			if r == conn.cluster.Spec.Region {
-				return nil
-			}
-		}
-		cloud.Logger(conn.ctx).Debug(".")
-		attempt++
-		time.Sleep(10 * time.Second)
-		if attempt > 60 {
-			break
-		}
-	}
-	return errors.New("Failed to transfer container os image.").WithContext(conn.ctx).Err()
 }
--- cloud/providers/gce/cloud.go
+++ cloud/providers/gce/cloud.go
@@ -3,7 +3,6 @@ package gce
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/appscode/go/errors"
 	"github.com/appscode/pharmer/api"
@@ -14,6 +13,7 @@ import (
 	compute "google.golang.org/api/compute/v1"
 	rupdate "google.golang.org/api/replicapoolupdater/v1beta1"
 	gcs "google.golang.org/api/storage/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 const containerOsImage = "appscode-containeros"
@@ -82,144 +82,6 @@ func (conn *cloudConnector) IsUnauthorized(project string) (bool, string) {
 	return false, ""
 }
 
-func (conn *cloudConnector) getInstanceImage() (string, error) {
-	_, err := conn.computeService.Images.Get(conn.cluster.Spec.Project, containerOsImage).Do()
-
-	if err != nil {
-		if !conn.checkDiskExists(containerOsImage) {
-			err = conn.createImageDisk(containerOsImage)
-			if err != nil {
-				return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-			}
-		}
-
-		cloud.Logger(conn.ctx).Infof("Creating %v image on %v project", containerOsImage, conn.cluster.Spec.Project)
-		r, err := conn.computeService.Images.Insert(conn.cluster.Spec.Project, &compute.Image{
-			Name:       containerOsImage,
-			SourceDisk: fmt.Sprintf("projects/%v/zones/%v/disks/%v", conn.cluster.Spec.Project, conn.cluster.Spec.Zone, containerOsImage),
-		}).Do()
-		if err != nil {
-			return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-		err = conn.waitForGlobalOperation(r.Name)
-		if err != nil {
-			return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-		cloud.Logger(conn.ctx).Infof("Image %v created", containerOsImage)
-		for {
-			fmt.Print("Pending image creation: ")
-			if r, err := conn.computeService.Images.Get(conn.cluster.Spec.Project, containerOsImage).Do(); err != nil || r.Status != "READY" {
-				fmt.Print("~")
-				time.Sleep(10 * time.Second)
-			} else {
-				break
-			}
-		}
-
-		_, err = conn.computeService.Disks.Delete(conn.cluster.Spec.Project, conn.cluster.Spec.Zone, containerOsImage).Do()
-		if err != nil {
-			return "", errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-	}
-	cloud.Logger(conn.ctx).Infof("Image %v found on project %v", containerOsImage, conn.cluster.Spec.Project)
-	return containerOsImage, nil
-}
-
-func (conn *cloudConnector) checkDiskExists(name string) bool {
-	_, err := conn.computeService.Disks.Get(conn.cluster.Spec.Project, conn.cluster.Spec.Zone, name).Do()
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-func (conn *cloudConnector) getSrcImage() (string, error) {
-	resp, err := conn.computeService.Images.List("debian-cloud").Do()
-	if err != nil {
-		return "", err
-	}
-	for _, image := range resp.Items {
-		if image.Family == "debian-8" && image.Deprecated == nil {
-			return image.Name, nil
-		}
-	}
-	return "", errors.New("Failed to detect debian-8 base image.").WithContext(conn.ctx).Err()
-}
-
-func (conn *cloudConnector) createImageDisk(name string) error {
-	machineType := fmt.Sprintf("projects/%v/zones/%v/machineTypes/n1-standard-1", conn.cluster.Spec.Project, conn.cluster.Spec.Zone)
-	img, err := conn.getSrcImage()
-	if err != nil || img == "" {
-		return errors.FromErr(err).WithMessage("No debian image found").WithContext(conn.ctx).Err()
-	}
-
-	srcImage := "projects/debian-cloud/global/images/" + img
-	startupScript := `#!/bin/bash
-sed -i 's/GRUB_CMDLINE_LINUX="console=ttyS0,38400n8 elevator=noop"/GRUB_CMDLINE_LINUX="console=ttyS0,38400n8 elevator=noop cgroup_enable=memory swapaccount=1"/' /etc/default/grub
-update-grub`
-	tempInstance := &compute.Instance{
-		Name:        name,
-		MachineType: machineType,
-		Disks: []*compute.AttachedDisk{
-			{
-				DeviceName: name,
-				Boot:       true,
-				AutoDelete: false,
-				InitializeParams: &compute.AttachedDiskInitializeParams{
-					SourceImage: srcImage,
-				},
-				Mode: "READ_WRITE",
-			},
-		},
-		NetworkInterfaces: []*compute.NetworkInterface{
-			{
-				Network: fmt.Sprintf("projects/%v/global/networks/%v", conn.cluster.Spec.Project, "default"),
-				AccessConfigs: []*compute.AccessConfig{
-					{
-						Type: "ONE_TO_ONE_NAT",
-						Name: "External NAT",
-					},
-				},
-			},
-		},
-		ServiceAccounts: []*compute.ServiceAccount{
-			{
-				Email: "default",
-				Scopes: []string{
-					compute.DevstorageReadWriteScope,
-				},
-			},
-		},
-		Metadata: &compute.Metadata{
-			Items: []*compute.MetadataItems{
-				{
-					Key:   "startup-script",
-					Value: &startupScript,
-				},
-			},
-		},
-	}
-	cloud.Logger(conn.ctx).Info("Creating instance for disk...")
-	r, err := conn.computeService.Instances.Insert(conn.cluster.Spec.Project, conn.cluster.Spec.Zone, tempInstance).Do()
-	if err != nil {
-		return errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-	conn.waitForZoneOperation(r.Name)
-	time.Sleep(1 * time.Minute)
-	cloud.Logger(conn.ctx).Info("Restarting instance ...")
-	r, err = conn.computeService.Instances.Reset(conn.cluster.Spec.Project, conn.cluster.Spec.Zone, name).Do()
-	if err != nil {
-		return errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-	conn.waitForZoneOperation(r.Name)
-	time.Sleep(1 * time.Minute)
-	err = conn.deleteInstance(name)
-	if err != nil {
-		return errors.FromErr(err).WithContext(conn.ctx).Err()
-	}
-	return nil
-}
-
 func (conn *cloudConnector) deleteInstance(name string) error {
 	cloud.Logger(conn.ctx).Info("Deleting instance...")
 	r, err := conn.computeService.Instances.Delete(conn.cluster.Spec.Project, conn.cluster.Spec.Zone, name).Do()
@@ -232,68 +94,51 @@ func (conn *cloudConnector) deleteInstance(name string) error {
 
 func (conn *cloudConnector) waitForGlobalOperation(operation string) error {
 	attempt := 0
-	for {
-		cloud.Logger(conn.ctx).Infof("Attempt %v: waiting for operation %v to complete", attempt, operation)
-		r1, err := conn.computeService.GlobalOperations.Get(conn.cluster.Spec.Project, operation).Do()
-		cloud.Logger(conn.ctx).Debug("Retrieved operation", r1, err)
-		if err != nil {
-			return errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-		if r1.Status == "DONE" {
-			return nil
-		}
-		cloud.Logger(conn.ctx).Infof("Attempt %v: operation %v is %v, waiting...", attempt, operation, r1.Status)
+	return wait.PollImmediate(cloud.RetryInterval, cloud.RetryTimeout, func() (bool, error) {
 		attempt++
-		time.Sleep(5 * time.Second)
-		if attempt > 120 {
-			break // 10 mins
+
+		r1, err := conn.computeService.GlobalOperations.Get(conn.cluster.Spec.Project, operation).Do()
+		if err != nil {
+			return false, nil
 		}
-	}
-	return errors.Newf("Global operation: %v failed to complete in 10 mins", operation).WithContext(conn.ctx).Err()
+		cloud.Logger(conn.ctx).Infof("Attempt %v: Operation %v is %v ...", attempt, operation, r1.Status)
+		if r1.Status == "DONE" {
+			return true, nil
+		}
+		return false, nil
+	})
 }
 
 func (conn *cloudConnector) waitForRegionOperation(operation string) error {
 	attempt := 0
-	for {
-		cloud.Logger(conn.ctx).Infof("Attempt %v: waiting for operation %v to complete", attempt, operation)
-		r1, err := conn.computeService.RegionOperations.Get(conn.cluster.Spec.Project, conn.cluster.Spec.Region, operation).Do()
-		cloud.Logger(conn.ctx).Debug("Retrieved operation", r1, err)
-		if err != nil {
-			return errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-
-		if r1.Status == "DONE" {
-			return nil
-		}
-		cloud.Logger(conn.ctx).Infof("Attempt %v: operation %v is %v, waiting...", attempt, operation, r1.Status)
+	return wait.PollImmediate(cloud.RetryInterval, cloud.RetryTimeout, func() (bool, error) {
 		attempt++
-		time.Sleep(5 * time.Second)
-		if attempt > 120 {
-			break // 10 mins
+
+		r1, err := conn.computeService.RegionOperations.Get(conn.cluster.Spec.Project, conn.cluster.Spec.Region, operation).Do()
+		if err != nil {
+			return false, nil
 		}
-	}
-	return errors.Newf("Region operation: %v failed to complete in 10 mins", operation).WithContext(conn.ctx).Err()
+		cloud.Logger(conn.ctx).Infof("Attempt %v: Operation %v is %v ...", attempt, operation)
+		if r1.Status == "DONE" {
+			return true, nil
+		}
+		return false, nil
+	})
 }
 
 func (conn *cloudConnector) waitForZoneOperation(operation string) error {
 	attempt := 0
-	for {
-		cloud.Logger(conn.ctx).Infof("Attempt %v: waiting for operation %v to complete", attempt, operation)
-		r1, err := conn.computeService.ZoneOperations.Get(conn.cluster.Spec.Project, conn.cluster.Spec.Zone, operation).Do()
-		cloud.Logger(conn.ctx).Debug("Retrieved operation", r1, err)
-		if err != nil {
-			return errors.FromErr(err).WithContext(conn.ctx).Err()
-		}
-
-		if r1.Status == "DONE" {
-			return nil //TODO check return value
-		}
-		cloud.Logger(conn.ctx).Infof("Attempt %v: operation %v is %v, waiting...", attempt, operation, r1.Status)
+	return wait.PollImmediate(cloud.RetryInterval, cloud.RetryTimeout, func() (bool, error) {
 		attempt++
-		time.Sleep(5 * time.Second)
-		if attempt > 120 {
-			break // 10 mins
+
+		r1, err := conn.computeService.ZoneOperations.Get(conn.cluster.Spec.Project, conn.cluster.Spec.Zone, operation).Do()
+		if err != nil {
+			return false, nil
 		}
-	}
-	return errors.Newf("Zone operation: %v failed to complete in 10 mins", operation).WithContext(conn.ctx).Err()
+		cloud.Logger(conn.ctx).Infof("Attempt %v: Operation %v is %v ...", attempt, operation)
+		if r1.Status == "DONE" {
+			return true, nil
+		}
+		return false, nil
+	})
 }
