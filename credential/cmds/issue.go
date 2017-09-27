12f15caf84f895c30860e729b9311a4d268c7b62
Tamal Saha <tamal@appscode.com>
2017-09-27 00:03:58 -0700

    Store issued credential (#125)

--- credential/cmds/issue.go
+++ credential/cmds/issue.go
@@ -1,11 +1,13 @@
 package cmds
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/appscode/go-term"
 	"github.com/appscode/pharmer/api"
-	"github.com/appscode/pharmer/credential/cloud"
+	"github.com/appscode/pharmer/cloud"
+	"github.com/appscode/pharmer/config"
+	cc "github.com/appscode/pharmer/credential/cloud"
 	"github.com/spf13/cobra"
 )
 
@@ -17,19 +19,36 @@ func NewCmdIssue() *cobra.Command {
 			api.ResourceCodeCredential,
 			api.ResourceKindCredential,
 		},
-		Short:             "Issue credential for cloud providers Azure and Google Cloud",
+		Short:             "Issue credential for cc providers Azure and Google Cloud",
 		Example:           `pharmer issue credential mycred`,
 		DisableAutoGenTag: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) != 1 {
-				term.Fatalln(fmt.Sprintf("You can only specify one argument, found %d", len(args)))
+			if len(args) == 0 {
+				term.Fatalln("Missing credential name.")
 			}
+			if len(args) > 1 {
+				term.Fatalln("Multiple credential name provided.")
+			}
+
+			cfgFile, _ := config.GetConfigFile(cmd.Flags())
+			cfg, err := config.LoadConfig(cfgFile)
+			if err != nil {
+				term.Fatalln(err)
+			}
+			ctx := cloud.NewContext(context.Background(), cfg)
 
 			provider, _ := cmd.Flags().GetString("provider")
 			if provider == "GoogleCloud" {
-				cloud.IssueGCECredential(args[0])
+				cc.IssueGCECredential(args[0])
 			} else if provider == "Azure" {
-				cloud.IssueAzureCredential(args[0])
+				cred, err := cc.IssueAzureCredential(args[0])
+				if err != nil {
+					term.Fatalln(err)
+				}
+				_, err = cloud.Store(ctx).Credentials().Create(cred)
+				if err != nil {
+					term.Fatalln(err)
+				}
 			}
 			term.Successln("Credential issued successfully!")
 		},
