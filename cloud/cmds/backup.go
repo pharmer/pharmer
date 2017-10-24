94f5a393a17af2cfa3ad294048920f92dc3a2562
tamal <tamal@appscode.com>
2017-10-24 14:27:22 -0700

    Use BackupManager from kutil

--- cloud/cmds/backup.go
+++ cloud/cmds/backup.go
@@ -1,23 +1,15 @@
 package cmds
 
 import (
-	"encoding/base64"
+	"context"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 
-	proto "github.com/appscode/api/kubernetes/v1beta1"
-	"github.com/appscode/appctl/pkg/config"
-	"github.com/appscode/appctl/pkg/util"
 	"github.com/appscode/go-term"
 	"github.com/appscode/go/flags"
-	"github.com/ghodss/yaml"
+	"github.com/appscode/kutil"
+	"github.com/appscode/pharmer/cloud"
+	"github.com/appscode/pharmer/config"
 	"github.com/spf13/cobra"
-	core "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/dynamic"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -27,280 +19,63 @@ type ItemList struct {
 	Items []map[string]interface{} `json:"items,omitempty"`
 }
 
-type backupOptions struct {
-	cluster   string
-	backupDir string
-	sanitize  bool
-}
-
 func NewCmdBackup() *cobra.Command {
-	var opts backupOptions
+	var (
+		clusterName string
+		backupDir   string
+		sanitize    bool
+	)
 	cmd := &cobra.Command{
 		Use:               "backup",
 		Short:             "Takes backup of YAML files of cluster",
 		DisableAutoGenTag: true,
 		Run: func(cmd *cobra.Command, args []string) {
 			flags.EnsureRequiredFlags(cmd, "cluster", "backup-dir")
-			restConfig, err := searchLocalKubeConfig(opts.cluster)
+
+			restConfig, err := searchLocalKubeConfig(clusterName)
 			if err != nil || restConfig == nil {
-				var clientConfigReq proto.ClusterClientConfigRequest
-				clientConfigReq.Name = opts.cluster
-				c := config.ClientOrDie()
-				resp, err := c.Kubernetes().V1beta1().Cluster().ClientConfig(c.Context(), &clientConfigReq)
-				util.PrintStatus(err)
-				restConfig, err = getConfigFromResp(resp)
-				util.PrintStatus(err)
+				cfgFile, _ := config.GetConfigFile(cmd.Flags())
+				cfg, err := config.LoadConfig(cfgFile)
+				if err != nil {
+					term.Fatalln(err)
+				}
+				ctx := cloud.NewContext(context.Background(), cfg, config.GetEnv(cmd.Flags()))
+
+				c2, err := cloud.GetAdminConfig(ctx, clusterName)
+				if err != nil {
+					term.Fatalln(err)
+				}
+				restConfig = &rest.Config{
+					Host: c2.Clusters[0].Cluster.Server,
+					TLSClientConfig: rest.TLSClientConfig{
+						CAData:   c2.Clusters[0].Cluster.CertificateAuthorityData,
+						CertData: c2.AuthInfos[0].AuthInfo.ClientCertificateData,
+						KeyData:  c2.AuthInfos[0].AuthInfo.ClientKeyData,
+					},
+				}
 			}
-			if err = ensureDirectory(opts.backupDir); err != nil {
+
+			mgr := kutil.NewBackupManager(clusterName, restConfig, sanitize)
+			err = mgr.BackupToTar(backupDir)
+			if err != nil {
 				term.Fatalln(err)
 			}
-			opts.getAndWriteAllObjectsFromCluster(restConfig)
+			term.Successln("Backup completed successfully!")
 		},
 	}
-	cmd.Flags().StringVarP(&opts.cluster, "cluster", "k", "", "Name of cluster or Kube config context")
-	cmd.Flags().BoolVar(&opts.sanitize, "sanitize", false, " Sanitize fields in YAML")
-	cmd.Flags().StringVar(&opts.backupDir, "backup-dir", "", "Directory where yaml files will be saved")
+	cmd.Flags().StringVarP(&clusterName, "cluster", "k", "", "Name of cluster or Kube config context")
+	cmd.Flags().BoolVar(&sanitize, "sanitize", false, " Sanitize fields in YAML")
+	cmd.Flags().StringVar(&backupDir, "backup-dir", "", "Directory where yaml files will be saved")
 	return cmd
 }
 
-func getConfigFromResp(resp *proto.ClusterClientConfigResponse) (*rest.Config, error) {
-	var err error
-	var tlsCfg rest.TLSClientConfig
-	if resp.CaCert != "" {
-		tlsCfg.CAData, err = base64.StdEncoding.DecodeString(resp.CaCert)
-		if err != nil {
-			return nil, err
-		}
-	}
-	if resp.UserCert != "" {
-		tlsCfg.CertData, err = base64.StdEncoding.DecodeString(resp.UserCert)
-		if err != nil {
-			return nil, err
-		}
-	}
-	if resp.UserKey != "" {
-		tlsCfg.KeyData, err = base64.StdEncoding.DecodeString(resp.UserKey)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	cfg := &rest.Config{
-		Host:            resp.ApiServerUrl,
-		TLSClientConfig: tlsCfg,
-	}
-	if resp.UserToken != "" {
-		cfg.BearerToken = resp.UserToken
-	} else if len(resp.ClusterUserName) > 0 && len(resp.Password) > 0 {
-		cfg.Password = resp.Password
-		cfg.Username = resp.ClusterUserName
-	}
-	return cfg, nil
-}
-
 func searchLocalKubeConfig(clusterName string) (*rest.Config, error) {
+	// ref: https://github.com/appscode/pharmer/blob/19db538fe51b83e807c525e2dbf28b56b7fb36e2/cloud/lib.go#L148
+	ctxName := fmt.Sprintf("cluster-admin@%s.pharmer", clusterName)
 	apiConfig, err := clientcmd.NewDefaultPathOptions().GetStartingConfig()
 	if err != nil {
 		return nil, err
 	}
-	overrides := &clientcmd.ConfigOverrides{CurrentContext: clusterName}
+	overrides := &clientcmd.ConfigOverrides{CurrentContext: ctxName}
 	return clientcmd.NewDefaultClientConfig(*apiConfig, overrides).ClientConfig()
-}
-
-func (backup backupOptions) getAndWriteAllObjectsFromCluster(kubeConfig *rest.Config) {
-	discoveryClient := discovery.NewDiscoveryClientForConfigOrDie(kubeConfig)
-	rs, err := discoveryClient.ServerResources()
-	if err != nil {
-		term.Fatalln(err)
-	}
-
-	err = os.MkdirAll(backup.backupDir, 0755)
-	if err != nil {
-		term.Fatalln(err)
-	}
-	resBytes, err := yaml.Marshal(rs)
-	if err != nil {
-		term.Fatalln(err)
-	}
-	err = ioutil.WriteFile(filepath.Join(backup.backupDir, "api_resources.yaml"), resBytes, 0755)
-	if err != nil {
-		term.Fatalln(err)
-	}
-
-	for _, v := range rs {
-		gv, err := schema.ParseGroupVersion(v.GroupVersion)
-		if err != nil {
-			continue
-		}
-		for _, rss := range v.APIResources {
-			term.Infoln("Taking backup for", rss.Name, "groupversion =", v.GroupVersion)
-			if err := rest.SetKubernetesDefaults(kubeConfig); err != nil {
-				term.Fatalln(err)
-			}
-			kubeConfig.ContentConfig = dynamic.ContentConfig()
-			kubeConfig.GroupVersion = &schema.GroupVersion{Group: gv.Group, Version: gv.Version}
-			kubeConfig.APIPath = "/apis"
-			if gv.Group == core.GroupName {
-				kubeConfig.APIPath = "/api"
-			}
-			restClient, err := rest.RESTClientFor(kubeConfig)
-			if err != nil {
-				term.Fatalln(err)
-			}
-			request := restClient.Get().Resource(rss.Name).Param("pretty", "true")
-			b, err := request.DoRaw()
-			if err != nil {
-				term.Errorln(err)
-				continue
-			}
-			list := &ItemList{}
-			err = yaml.Unmarshal(b, &list)
-			if err != nil {
-				term.Errorln(err)
-				continue
-			}
-			if len(list.Items) > 1000 {
-				ok := term.Ask(fmt.Sprintf("Too many objects (%v). Want to take backup ?", len(list.Items)), true)
-				if !ok {
-					continue
-				}
-			}
-			for _, ob := range list.Items {
-				var selfLink string
-				ob["apiVersion"] = v.GroupVersion
-				ob["kind"] = rss.Kind
-				i, ok := ob["metadata"]
-				if ok {
-					selfLink = getSelfLinkFromMetadata(i)
-				} else {
-					term.Errorln("Metadata not found")
-					continue
-				}
-				if backup.sanitize {
-					cleanUpObjectMeta(i)
-					spec, ok := ob["spec"].(map[string]interface{})
-					if ok {
-						if rss.Kind == "Pod" {
-							spec = cleanUpPodSpec(spec)
-						}
-						template, ok := spec["template"].(map[string]interface{})
-						if ok {
-							podSpec, ok := template["spec"].(map[string]interface{})
-							if ok {
-								template["spec"] = cleanUpPodSpec(podSpec)
-							}
-						}
-					}
-					delete(ob, "status")
-				}
-				b, err := yaml.Marshal(ob)
-				if err != nil {
-					term.Errorln(err)
-					break
-				}
-				path := filepath.Dir(filepath.Join(backup.backupDir, selfLink))
-				obName := filepath.Base(selfLink)
-				err = os.MkdirAll(path, 0777)
-				if err != nil {
-					term.Errorln(err)
-					break
-				}
-				fileName := filepath.Join(path, obName+".yaml")
-				if err = ioutil.WriteFile(fileName, b, 0644); err != nil {
-					term.Errorln(err)
-					continue
-				}
-
-			}
-		}
-	}
-}
-
-func ensureDirectory(dir string) error {
-	err := os.MkdirAll(dir, 0777)
-	return err
-}
-
-func cleanUpObjectMeta(i interface{}) {
-	meta, ok := i.(map[string]interface{})
-	if !ok {
-		return
-	}
-	delete(meta, "creationTimestamp")
-	delete(meta, "resourceVersion")
-	delete(meta, "selfLink")
-	delete(meta, "uid")
-	delete(meta, "generateName")
-	delete(meta, "generation")
-	annotation, ok := meta["annotations"]
-	if !ok {
-		return
-	}
-	annotations, ok := annotation.(map[string]string)
-	if !ok {
-		return
-	}
-	cleanUpDecorators(annotations)
-}
-
-func cleanUpDecorators(i interface{}) {
-	m, ok := i.(map[string]interface{})
-	if !ok {
-		return
-	}
-	delete(m, "controller-uid")
-	delete(m, "deployment.kubernetes.io/desired-replicas")
-	delete(m, "deployment.kubernetes.io/max-replicas")
-	delete(m, "deployment.kubernetes.io/revision")
-	delete(m, "pod-template-hash")
-	delete(m, "pv.kubernetes.io/bind-completed")
-	delete(m, "pv.kubernetes.io/bound-by-controller")
-}
-
-func cleanUpPodSpec(podSpec map[string]interface{}) map[string]interface{} {
-	b, err := yaml.Marshal(podSpec)
-	if err != nil {
-		term.Errorln(err)
-		return podSpec
-	}
-	p := &core.PodSpec{}
-	err = yaml.Unmarshal(b, p)
-	if err != nil {
-		term.Errorln(err)
-		return podSpec // Not a podspec
-	}
-	p.DNSPolicy = core.DNSPolicy("")
-	p.NodeName = ""
-	if p.ServiceAccountName == "default" {
-		p.ServiceAccountName = ""
-	}
-	p.TerminationGracePeriodSeconds = nil
-	for i, c := range p.Containers {
-		c.TerminationMessagePath = ""
-		p.Containers[i] = c
-	}
-	for i, c := range p.InitContainers {
-		c.TerminationMessagePath = ""
-		p.InitContainers[i] = c
-	}
-	b, err = yaml.Marshal(p)
-	if err != nil {
-		term.Errorln(err)
-		return nil
-	}
-	var spec map[string]interface{}
-	err = yaml.Unmarshal(b, &spec)
-	if err != nil {
-		term.Errorln(err)
-		return spec
-	}
-	return spec
-}
-
-func getSelfLinkFromMetadata(i interface{}) string {
-	meta, ok := i.(map[string]interface{})
-	if ok {
-		return meta["selfLink"].(string)
-	}
-	return ""
 }
