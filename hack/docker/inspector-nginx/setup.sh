7f0eb69c36c8e1b59be740dae3d39ee3b61322f7
tamal <tamal@appscode.com>
2017-10-25 23:06:18 -0700

    Add inspector nginx image

---
+++ hack/docker/inspector-nginx/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eou pipefail
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT=$GOPATH/src/github.com/appscode/pharmer
+
+source "$REPO_ROOT/hack/libbuild/common/public_image.sh"
+
+IMG=inspector-nginx
+TAG=alpine
+
+build() {
+	pushd $(dirname "${BASH_SOURCE}")
+	local cmd="docker build -t appscode/$IMG:$TAG ."
+	echo $cmd; $cmd
+	popd
+}
+
+binary_repo $@
