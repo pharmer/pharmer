4df3e52ed40a000eebe54503385416dd7f791209
Tamal Saha <tamal@appscode.com>
2017-11-01 17:11:15 -0700

    Use lockfile to avoid multiple execution of installer in softlayer (#208)

--- cloud/providers/softlayer/startupscript.sh
+++ cloud/providers/softlayer/startupscript.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-# ref: https://stackoverflow.com/a/2831449/244009
-cd /tmp
-/usr/bin/wget -O kubeadm.sh https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/UserMetadata.txt
-chmod +x kubeadm.sh
-nohup ./kubeadm.sh > /dev/null 2>&1 &
+# ref: https://gist.github.com/przemoc/571091
+# ref: https://stackoverflow.com/a/1985512/244009
 
-exit 0
+LOCKFILE="/var/lock/pharmer"
+LOCKFD=99
+
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+
+# ON START
+_prepare_locking
+
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x; }   # obtain an exclusive lock
+shlock()            { _lock s; }   # obtain a shared lock
+unlock()            { _lock u; }   # drop a lock
+
+### BEGIN OF SCRIPT ###
+
+# Simplest example is avoiding running multiple instances of script.
+exlock_now || exit 1
+
+/usr/bin/wget -O /usr/bin/pharmer.sh https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/UserMetadata.txt
+chmod +x /usr/bin/pharmer.sh
+/usr/bin/pharmer.sh
+
+# Remember! Lock file is removed when one of the scripts exits and it is
+#           the only script holding the lock or lock is not acquired at all.
