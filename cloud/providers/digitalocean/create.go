d1c6325404a997f810a607f5e9687533b2fa2916
Tamal Saha <tamal@appscode.com>
2017-11-08 04:07:53 -0800

    Add setter methods for context params (#242)

--- cloud/providers/digitalocean/create.go
+++ cloud/providers/digitalocean/create.go
@@ -41,7 +41,7 @@ func (cm *ClusterManager) SetDefaults(cluster *api.Cluster) error {
 	cluster.Spec.Cloud.InstanceImage = "ubuntu-16-04-x64"
 	cluster.Spec.Networking.SetDefaults()
 	cluster.Spec.AuthorizationModes = strings.Split(kubeadmapi.DefaultAuthorizationModes, ",")
-	cluster.Spec.APIServerCertSANs = Extra(cm.ctx).ExtraNames(cluster.Name)
+	cluster.Spec.APIServerCertSANs = NameGenerator(cm.ctx).ExtraNames(cluster.Name)
 	cluster.Spec.APIServerExtraArgs = map[string]string{
 		// ref: https://github.com/kubernetes/kubernetes/blob/d595003e0dc1b94455d1367e96e15ff67fc920fa/cmd/kube-apiserver/app/options/options.go#L99
 		"kubelet-preferred-address-types": strings.Join([]string{
