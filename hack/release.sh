77cb29a26eba0c3cc06775a9e13fa61acf54f3aa
tamal <tamal@appscode.com>
2017-11-27 12:28:08 -0800

    Simplify release.sh

--- hack/release.sh
+++ hack/release.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+set -xeou pipefail
 
-set -o errexit
-set -o nounset
-set -o pipefail
+GOPATH=$(go env GOPATH)
+REPO_ROOT="$GOPATH/src/github.com/appscode/pharmer"
 
 export APPSCODE_ENV=prod
 
-pushd "$(go env GOPATH)/src/github.com/appscode/pharmer"
+pushd $REPO_ROOT
+
 rm -rf dist
+
 ./hack/make.py build
 ./hack/make.py push
 ./hack/make.py update_registry
+
+rm dist/.tag
+
 popd
