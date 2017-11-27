f8d3a685695fe838174ba54aba3eb42db681956c
Tamal Saha <tamal@appscode.com>
2017-11-27 12:14:51 -0800

    Update release.sh

--- hack/release.sh
+++ hack/release.sh
@@ -6,8 +6,7 @@ set -o pipefail
 
 pushd "$(go env GOPATH)/src/github.com/appscode/pharmer"
 rm -rf dist
-./hack/make.py build
-./hack/make.py push
+APPSCODE_ENV=prod ./hack/make.py build
 APPSCODE_ENV=prod ./hack/make.py push
 ./hack/make.py update_registry
 popd
