db162094e68fbe30ee12d30dbaf4c311a9522e79
tamal <tamal@appscode.com>
2018-05-03 17:04:01 -0700

    Update dependencies to Kubernetes 1.10

--- vendor/k8s.io/apimachinery/pkg/apis/meta/v1/time.go
+++ vendor/k8s.io/apimachinery/pkg/apis/meta/v1/time.go
@@ -20,10 +20,8 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/go-openapi/spec"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/google/gofuzz"
-	openapi "k8s.io/kube-openapi/pkg/common"
 )
 
 // Time is a wrapper around time.Time which supports correct
@@ -159,16 +157,15 @@ func (t *Time) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, jstr []byte) error {
 	return t.UnmarshalJSON(jstr)
 }
 
-func (_ Time) OpenAPIDefinition() openapi.OpenAPIDefinition {
-	return openapi.OpenAPIDefinition{
-		Schema: spec.Schema{
-			SchemaProps: spec.SchemaProps{
-				Type:   []string{"string"},
-				Format: "date-time",
-			},
-		},
-	}
-}
+// OpenAPISchemaType is used by the kube-openapi generator when constructing
+// the OpenAPI spec of this type.
+//
+// See: https://github.com/kubernetes/kube-openapi/tree/master/pkg/generators
+func (_ Time) OpenAPISchemaType() []string { return []string{"string"} }
+
+// OpenAPISchemaFormat is used by the kube-openapi generator when constructing
+// the OpenAPI spec of this type.
+func (_ Time) OpenAPISchemaFormat() string { return "date-time" }
 
 // MarshalQueryParameter converts to a URL query parameter value
 func (t Time) MarshalQueryParameter() (string, error) {
