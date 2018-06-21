1844419e5a38967b6729d85e1cd637658af9e27b
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-06-21 10:28:20 +0600

    concourse tests (#404)

--- hack/builddeps.sh
+++ hack/builddeps.sh
@@ -4,6 +4,7 @@
 pip install git+https://github.com/ellisonbg/antipackage.git#egg=antipackage
 pip install pyyaml
 
+go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/appscode/go-bindata/...
 go get -u golang.org/x/tools/cmd/goimports
 go get github.com/Masterminds/glide
---
+++ hack/concourse/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x -e
+
+#install python pip
+apt-get update > /dev/null
+apt-get install -y python python-pip > /dev/null
+
+#copy pharmer to $GOPATH
+mkdir -p $GOPATH/src/github.com/pharmer
+cp -r pharmer $GOPATH/src/github.com/pharmer
+pushd $GOPATH/src/github.com/pharmer/pharmer
+
+#build pharmer
+./hack/builddeps.sh
+./hack/make.py
+
+popd
+
+#copy pharmer here for uploading to s3
+cp $GOPATH/bin/pharmer .
