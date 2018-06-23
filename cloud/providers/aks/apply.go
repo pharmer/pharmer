3d6b4b0972a635afbe9641e76c95cc5a106c3bc1
sanjid133 <sanjid@appscode.com>
2018-06-23 19:03:56 +0600

    Azure aks documentation added

--- cloud/providers/aks/apply.go
+++ cloud/providers/aks/apply.go
@@ -78,21 +78,6 @@ func (cm *ClusterManager) Apply(in *api.Cluster, dryRun bool) ([]api.Action, err
 		acts = append(acts, a...)
 	}
 	return acts, nil
-
-	/*defer func(releaseReservedIp bool) {
-		if cm.cluster.Status.Phase == api.ClusterPending {
-			cm.cluster.Status.Phase = api.ClusterFailing
-		}
-		Store(cm.ctx).Clusters().UpdateStatus(cm.cluster)
-		Logger(cm.ctx).Infof("Cluster %v is %v", cm.cluster.Name, cm.cluster.Status.Phase)
-		if cm.cluster.Status.Phase != api.ClusterReady {
-			Logger(cm.ctx).Infof("Cluster %v is deleting", cm.cluster.Name)
-			cm.Delete(&proto.ClusterDeleteRequest{
-				Name:              cm.cluster.Name,
-				ReleaseReservedIp: releaseReservedIp,
-			})
-		}
-	}(cm.cluster.Spec.MasterReservedIP == "auto")*/
 }
 
 // IP >>>>>>>>>>>>>>>>
--- cloud/providers/aks/context.go
+++ cloud/providers/aks/context.go
@@ -12,8 +12,10 @@ import (
 	//"k8s.io/client-go/util/cert"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 
 	"github.com/ghodss/yaml"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientcmd "k8s.io/client-go/tools/clientcmd/api/v1"
 )
 
@@ -90,4 +92,62 @@ func (cm *ClusterManager) GetAKSAdminClient() (kubernetes.Interface, error) {
 	}
 	return kubernetes.NewForConfig(cfg)
 
+}
+
+func (cm *ClusterManager) GetKubeConfig(cluster *api.Cluster) (*api.KubeConfig, error) {
+	var err error
+	cm.cluster = cluster
+	cm.namer = namer{cluster: cm.cluster}
+	if cm.conn, err = NewConnector(cm.ctx, cm.cluster); err != nil {
+		return nil, err
+	}
+
+	resp, err := cm.conn.managedClient.GetAccessProfiles(context.Background(), cm.namer.ResourceGroupName(), cm.cluster.Name, RoleClusterUser)
+	if err != nil {
+		return nil, err
+	}
+	kubeconfig, err := base64.StdEncoding.DecodeString(*resp.KubeConfig)
+	if err != nil {
+		return nil, err
+	}
+	kubeconfig, err = yaml.YAMLToJSON(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	var konfig clientcmd.Config
+	err = json.Unmarshal(kubeconfig, &konfig)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		clusterName = fmt.Sprintf("%s.pharmer", cluster.Name)
+		userName    = fmt.Sprintf("cluster-admin@%s.pharmer", cluster.Name)
+		ctxName     = fmt.Sprintf("cluster-admin@%s.pharmer", cluster.Name)
+	)
+	cfg := api.KubeConfig{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "KubeConfig",
+		},
+		Preferences: api.Preferences{
+			Colors: true,
+		},
+		Cluster: api.NamedCluster{
+			Name:   clusterName,
+			Server: konfig.Clusters[0].Cluster.Server,
+			CertificateAuthorityData: konfig.Clusters[0].Cluster.CertificateAuthorityData,
+		},
+		AuthInfo: api.NamedAuthInfo{
+			Name: userName,
+			ClientCertificateData: konfig.AuthInfos[0].AuthInfo.ClientCertificateData,
+			ClientKeyData:         konfig.AuthInfos[0].AuthInfo.ClientKeyData,
+		},
+		Context: api.NamedContext{
+			Name:     ctxName,
+			Cluster:  clusterName,
+			AuthInfo: userName,
+		},
+	}
+	return &cfg, nil
 }
