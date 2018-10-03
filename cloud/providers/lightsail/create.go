3cdea9d752ddb582dca2420a138f63495d46aa52
sanjid133 <sanjid@appscode.com>
2018-10-03 16:44:15 +0600

    lightsail add

--- cloud/providers/lightsail/create.go
+++ cloud/providers/lightsail/create.go
@@ -11,8 +11,7 @@ import (
 	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
-	kubeadmapialpha1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha1"
-	kubeadmapi "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha2"
+	kubeadmapi "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha3"
 )
 
 func (cm *ClusterManager) GetDefaultNodeSpec(cluster *api.Cluster, sku string) (api.NodeSpec, error) {
@@ -41,9 +40,7 @@ func (cm *ClusterManager) SetDefaults(cluster *api.Cluster) error {
 	cluster.Spec.API.BindPort = kubeadmapi.DefaultAPIBindPort
 	// cluster.Spec.Cloud.InstanceImage = "ubuntu_16_04_1"
 	cluster.Spec.Networking.SetDefaults()
-	if cluster.IsLessThanVersion("1.11.0") {
-		cluster.Spec.AuthorizationModes = strings.Split(kubeadmapialpha1.DefaultAuthorizationModes, ",")
-	}
+
 	cluster.Spec.APIServerCertSANs = NameGenerator(cm.ctx).ExtraNames(cluster.Name)
 	cluster.Spec.APIServerExtraArgs = map[string]string{
 		// ref: https://github.com/kubernetes/kubernetes/blob/d595003e0dc1b94455d1367e96e15ff67fc920fa/cmd/kube-apiserver/app/options/options.go#L99
